24215033047724692b79a1a3ed19c8b698a0360d
SitananYaowayang <6733270421@student.chula.ac.th>
2025-02-27 15:31:42 +0700

    Update router.use in routes\companies

--- routes/companies.js
+++ routes/companies.js
@@ -10,7 +10,8 @@ const router = express.Router();
 
 const {protect,authorize} = require('../middleware/auth');
 
-router.use('/:companyId/interviewsessions/',appointmentRouter);
+router.use('/:companyId/interviewsessions/', interviewSessionRouter);
+router.use('/:companyId/bookings/', bookingRouter);
 
 router.route("/").get(getCompanies).post(protect, authorize('admin'), createCompany);
 router.route("/:id").get(getCompany).put(protect, authorize('admin'), updateCompany).delete(protect, authorize('admin'), deleteCompany);
