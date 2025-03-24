b2bf86ed0ca2b340225f2338e5eee568956f502d
Nutdanai Jaratwiboonkul <ozone4826@gmail.com>
2025-03-25 01:37:15 +0700

    ozone-booking

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -68,46 +68,46 @@ exports.getBookings = async (req, res, next) => {
         }
     }
     try {
-        if (req.query.select) {
-            const fields = req.query.select.split(",").join(" ");
-            query = query.select(fields);
-        }
+        // if (req.query.select) {
+        //     const fields = req.query.select.split(",").join(" ");
+        //     query = query.select(fields);
+        // }
         
-        if (req.query.sort) {
-            const sortBy = req.query.sort.split(",").join(" ");
-            query = query.sort(sortBy);
-        } 
+        // if (req.query.sort) {
+        //     const sortBy = req.query.sort.split(",").join(" ");
+        //     query = query.sort(sortBy);
+        // } 
         
 
-        const page = parseInt(req.query.page, 10) || 1;
-        const limit = parseInt(req.query.limit, 10) || 25;
-        const startIndex = (page - 1) * limit;
-        const endIndex = page * limit;
+        // const page = parseInt(req.query.page, 10) || 1;
+        // const limit = parseInt(req.query.limit, 10) || 25;
+        // const startIndex = (page - 1) * limit;
+        // const endIndex = page * limit;
 
-        const total = await Company.countDocuments();
+        // const total = await Company.countDocuments();
 
-        query = query.skip(startIndex).limit(limit);
+        // query = query.skip(startIndex).limit(limit);
 
         const bookings = await query;
 
-        const pagination = {};
-        if (endIndex < total) {
-            pagination.next = {
-                page: page + 1,
-                limit
-            };
-        }
-        if (startIndex > 0) {
-            pagination.prev = {
-                page: page - 1,
-                limit
-            };
-        }
+        // const pagination = {};
+        // if (endIndex < total) {
+        //     pagination.next = {
+        //         page: page + 1,
+        //         limit
+        //     };
+        // }
+        // if (startIndex > 0) {
+        //     pagination.prev = {
+        //         page: page - 1,
+        //         limit
+        //     };
+        // }
 
         res.status(200).json({
             success: true,
             count: bookings.length,
-            pagination,
+            // pagination,
             data: bookings
         });
     } catch (error) {
